3f68fa522ae52e0769e7bf935eb74a3bdf18c7b5
Dauemi <88590904+Dauemi@users.noreply.github.com>
2024-08-08 14:54:05 +0100

    Update 1-ExerciseOperatorPrecedence.py

--- 2. Python Basics/1-ExerciseOperatorPrecedence.py
+++ 2. Python Basics/1-ExerciseOperatorPrecedence.py
@@ -1,12 +1,12 @@
 # Guess the output of each answer before you click RUN
 # Try to write down your answer before and see how you do... keep it mind I made it a little tricky for you :)
 
-print((5 + 4) * 10 / 2)
+print((5 + 4) * 10 / 2) 45
 
-print(((5 + 4) * 10) / 2)
+print(((5 + 4) * 10) / 2) 45.0
 
-print((5 + 4) * (10 / 2))
+print((5 + 4) * (10 / 2)) 45
 
-print(5 + (4 * 10) / 2)
+print(5 + (4 * 10) / 2) 25
 
-print(5 + 4 * 10 // 2)
+print(5 + 4 * 10 // 2) 25
